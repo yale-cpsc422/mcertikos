1dfb3ac7d90bfe80e6a2e974722abd8b5f243e44
Haozhong Zhang <haozhong.zhang@yale.edu>
2013-05-27 16:17:51 -0400

    Compile sys/lib/string.h by CompCert.

--- sys/lib/string.c
+++ sys/lib/string.c
@@ -2,16 +2,6 @@
 #include <sys/types.h>
 
 int
-strlen(const char *s)
-{
-	int n;
-
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return n;
-}
-
-int
 strnlen(const char *s, size_t size)
 {
 	int n;
@@ -19,17 +9,6 @@ strnlen(const char *s, size_t size)
 	for (n = 0; size > 0 && *s != '\0'; s++, size--)
 		n++;
 	return n;
-}
-
-char *
-strcpy(char *dst, const char *src)
-{
-	char *ret;
-
-	ret = dst;
-	while ((*dst++ = *src++) != '\0')
-		/* do nothing */;
-	return ret;
 }
 
 char *
@@ -48,28 +27,6 @@ strncpy(char *dst, const char *src, size_t size)
 	return ret;
 }
 
-size_t
-strlcpy(char *dst, const char *src, size_t size)
-{
-	char *dst_in;
-
-	dst_in = dst;
-	if (size > 0) {
-		while (--size > 0 && *src != '\0')
-			*dst++ = *src++;
-		*dst = '\0';
-	}
-	return dst - dst_in;
-}
-
-int
-strcmp(const char *p, const char *q)
-{
-	while (*p && *p == *q)
-		p++, q++;
-	return (int) ((unsigned char) *p - (unsigned char) *q);
-}
-
 int
 strncmp(const char *p, const char *q, size_t n)
 {
@@ -81,87 +38,12 @@ strncmp(const char *p, const char *q, size_t n)
 		return (int) ((unsigned char) *p - (unsigned char) *q);
 }
 
-char *
-strchr(const char *s, char c)
-{
-	for (; *s; s++)
-		if (*s == c)
-			return (char *) s;
-	return 0;
-}
-
-char *
-strfind(const char *s, char c)
-{
-	for (; *s; s++)
-		if (*s == c)
-			break;
-	return (char *) s;
-}
-
-
-long
-strtol(const char *s, char **endptr, int base)
-{
-	int neg = 0;
-	long val = 0;
-
-	// gobble initial whitespace
-	while (*s == ' ' || *s == '\t')
-		s++;
-
-	// plus/minus sign
-	if (*s == '+')
-		s++;
-	else if (*s == '-')
-		s++, neg = 1;
-
-	// hex or octal base prefix
-	if ((base == 0 || base == 16) && (s[0] == '0' && s[1] == 'x'))
-		s += 2, base = 16;
-	else if (base == 0 && s[0] == '0')
-		s++, base = 8;
-	else if (base == 0)
-		base = 10;
-
-	// digits
-	while (1) {
-		int dig;
-
-		if (*s >= '0' && *s <= '9')
-			dig = *s - '0';
-		else if (*s >= 'a' && *s <= 'z')
-			dig = *s - 'a' + 10;
-		else if (*s >= 'A' && *s <= 'Z')
-			dig = *s - 'A' + 10;
-		else
-			break;
-		if (dig >= base)
-			break;
-		s++, val = (val * base) + dig;
-		// we don't properly detect overflow!
-	}
-
-	if (endptr)
-		*endptr = (char *) s;
-	return (neg ? -val : val);
-}
-
 void *
 memset(void *v, int c, size_t n)
 {
-	if (n == 0)
-		return v;
-	if ((int)v%4 == 0 && n%4 == 0) {
-		c &= 0xFF;
-		c = (c<<24)|(c<<16)|(c<<8)|c;
-		asm volatile("cld; rep stosl\n"
-			     :: "D" (v), "a" (c), "c" (n/4)
-			     : "cc", "memory");
-	} else
-		asm volatile("cld; rep stosb\n"
-			     :: "D" (v), "a" (c), "c" (n)
-			     : "cc", "memory");
+	char *ptr = v;
+	while (n--)
+		ptr[n] = c;
 	return v;
 }
 
@@ -180,28 +62,14 @@ memmove(void *dst, const void *src, size_t n)
 	s = src;
 	d = dst;
 	if (s < d && s + n > d) {
-		s += n;
-		d += n;
-		if ((int)s%4 == 0 && (int)d%4 == 0 && n%4 == 0)
-			asm volatile("std; rep movsl\n"
-				     :: "D" (d-4), "S" (s-4), "c" (n/4)
-				     : "cc", "memory");
-		else
-			asm volatile("std; rep movsb\n"
-				     :: "D" (d-1), "S" (s-1), "c" (n)
-				     : "cc", "memory");
-		// Some versions of GCC rely on DF being clear
-		asm volatile("cld" ::: "cc");
+		while (n--)
+			d[n] = s[n];
 	} else {
-		if ((int)s%4 == 0 && (int)d%4 == 0 && n%4 == 0)
-			asm volatile("cld; rep movsl\n"
-				     :: "D" (d), "S" (s), "c" (n/4)
-				     : "cc", "memory");
-		else
-			asm volatile("cld; rep movsb\n"
-				     :: "D" (d), "S" (s), "c" (n)
-				     : "cc", "memory");
+		int i;
+		for (i = 0; i < n; i++)
+			d[i] = s[i];
 	}
+
 	return dst;
 }
 
--- sys/sys/string.h
+++ sys/sys/string.h
@@ -7,17 +7,9 @@
 
 #include <sys/types.h>
 
-int     strlen(const char *s);
 int     strnlen(const char *s, size_t size);
-char *  strcpy(char *dst, const char *src);
 char *  strncpy(char *dst, const char *src, size_t size);
-size_t  strlcpy(char *dst, const char *src, size_t size);
-int     strcmp(const char *s1, const char *s2);
 int     strncmp(const char *s1, const char *s2, size_t size);
-char *  strchr(const char *s, char c);
-char *  strfind(const char *s, char c);
-long    strtol(const char *s, char **endptr, int base);
-char *  strerror(int err);
 
 void *	memset(void *dst, int c, size_t len);
 void *	memzero(void *dst, size_t len);
