38be7c2a61a27b9ac22a5acf503c770d7222e44e
Haozhong Zhang <snowzh8512@gmail.com>
2013-09-11 15:42:47 -0400

    Add the top-level initialization.

--- sys/kern/init/init.c
+++ sys/kern/init/init.c
@@ -22,9 +22,15 @@ kern_main(void)
 	if (idle_proc == NULL)
 		KERN_PANIC("Cannot create the idle process.\n");
 
+	KERN_DEBUG("idle process %d is created.\n", idle_proc->pid);
+
 	guest_proc = proc_create((uintptr_t) _binary___obj_user_vmm_vmm_start);
 	if (guest_proc == NULL)
 		KERN_PANIC("Cannot create the VMM process.\n");
+
+	KERN_DEBUG("vmm process %d is created.\n", guest_proc->pid);
+
+	KERN_INFO("Start user-space ... \n");
 
 	thread_sched();
 
@@ -32,7 +38,7 @@ kern_main(void)
 }
 
 void
-kern_init(struct mboot_info *mbi)
+kern_init(uintptr_t mbi_addr)
 {
 	/*
 	 * Setup segments.
@@ -52,39 +58,50 @@ kern_init(struct mboot_info *mbi)
 	 * debug messages to the screen and/or the serial port.
 	 */
 	cons_init();
+	KERN_INFO("Console is ready.\n");
+
 
 	/*
 	 * Detect CPU.
 	 */
+	KERN_INFO("Detect CPU and APIC ... ");
 	pcpu_init();
-
-	/*
-	 * Initialize the physical memory allocator.
-	 */
-	mem_init(mbi);
+	KERN_INFO("done.\n");
 
 	/*
 	 * Initialize the virtual memory.
 	 */
-	pmap_init();
+	KERN_INFO("Initialize memory management module ... ");
+	pmap_init(mbi_addr);
+	KERN_INFO("done.\n");
 
 	/*
 	 * Initialize the interrupt controllers.
 	 */
+	KERN_INFO("Initialize interrupt controller ... ");
 	intr_init();
+	KERN_INFO("done.\n");
 
 	/*
 	 * Initialize the process management.
 	 */
+	KERN_INFO("Initialize process management module ... ");
 	proc_init();
+	KERN_INFO("done.\n");
 
 	/*
 	 * Initialize the hardware virtualization driver.
 	 */
+	KERN_INFO("Initialize virtualization module ... ");
 	hvm_init();
+	KERN_INFO("done.\n");
 
 	/*
 	 * Initialize the IDE disk driver.
 	 */
+	KERN_INFO("Detect IDE disk driver ... ");
 	ide_init();
+	KERN_INFO("done.\n");
+
+	kern_main();
 }
