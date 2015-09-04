808ca90ab56bd81728d2b6821d671abb4a453688
Newman Wu <xiongnan.wu@yale.edu>
2015-09-04 12:53:34 -0400

    Fix

--- sys/preinit/preinit.c
+++ sys/preinit/preinit.c
@@ -32,45 +32,36 @@ set_vendor()
 void
 preinit (uintptr_t mbi_addr)
 {
-	FENCE();
     seg_init ();
 
-    FENCE();
     enable_sse ();
 
-    FENCE();
     cons_init ();
     KERN_DEBUG("cons initialized.\n");
 
-    FENCE();
     tsc_init ();
     KERN_DEBUG("tsc initialized.\n");
 
 #ifdef PROFILING
-    FENCE();
     profiling_init ();
     KERN_DEBUG("profiling initialized.\n");
 #endif
 
-    FENCE();
     sysenter_init();
     KERN_DEBUG("sysenter initialized.\n");
 
-    FENCE();
     intr_init ();
     KERN_DEBUG("intr initialized.\n");
 
     /* 	ide_init(); */
     /* KERN_DEBUG("ide initialized.\n"); */
 
-    FENCE();
     disk_init ();
 
-    FENCE();
     pci_init ();
 
-    FENCE();
     set_vendor ();
+    /*
     if (cpuvendor == AMD)
     {
         KERN_DEBUG("vendor detected: AMD.\n");
@@ -86,13 +77,12 @@ preinit (uintptr_t mbi_addr)
     {
         KERN_PANIC("unknown cpu vendor.\n");
     }
+    */
 
     /* enable interrupts */
-    FENCE();
     intr_enable (IRQ_TIMER);
     intr_enable (IRQ_KBD);
     intr_enable (IRQ_SERIAL13);
 
-    FENCE();
     pmmap_init (mbi_addr);
 }
