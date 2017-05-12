79b063a8f08ef3c5fcab65d7fdc94fea07bdbbf2
Hao Chen <hao.chen@yale.edu>
2017-05-12 09:15:52 +0800

    gcc compiler

--- misc/gccprefix.sh
+++ misc/gccprefix.sh
@@ -11,6 +11,8 @@
 
 if pios-objdump -i 2>&1 | grep '^elf32-i386$' >/dev/null 2>&1; then
 	echo 'pios-'
+elif i686-elf-objdump -i 2>&1 | grep '^elf32-i386$' >/dev/null 2>&1; then
+	echo 'i686-elf-'
 elif i386-elf-objdump -i 2>&1 | grep '^elf32-i386$' >/dev/null 2>&1; then
 	echo 'i386-elf-'
 elif objdump -i 2>&1 | grep 'elf32-i386' >/dev/null 2>&1; then
