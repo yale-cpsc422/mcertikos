8dc9ad4cf7ef6ff5c9dd2b1811e351b9a2e6bec2
Yixuan Chen <yixuan.chen@yale.edu>
2020-09-19 18:53:44 -0400

    import container_free for MPTNew

--- kern/vmm/MPTNew/import.h
+++ kern/vmm/MPTNew/import.h
@@ -4,6 +4,7 @@
 #ifdef _KERN_
 
 unsigned int container_alloc(unsigned int id);
+void container_free(unsigned int id, unsigned int page_index);
 unsigned int container_split(unsigned int id, unsigned int quota);
 unsigned int map_page(unsigned int proc_index, unsigned int vaddr,
                       unsigned int page_index, unsigned int perm);
